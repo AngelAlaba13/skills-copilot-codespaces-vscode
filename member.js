66f914f82a3043d9bbda62ebfa990943d5cdbac7
Angel <126352711+AngelAlaba13@users.noreply.github.com>
2025-04-12 23:34:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>HTML & CSS</li>
+        <li>Git & GitHub</li>
+      </ul>
+    </div>
+  );
+}
